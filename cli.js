2b42474e80fad4a69b976351cc070bd7ec280189
王晓东 <1099079410@qq.com>
2020-09-08 07:15:59 +0000

    1

--- cli.js
+++ cli.js
@@ -1,8 +1,39 @@
 #! /usr/bin/env node
 const fs = require('fs')
-const path = require('path')
+const join = require('path').join
 const inquirer = require('inquirer')
 const ejs = require('ejs')
+
+function fs_mkdirs(destDir) {
+    fs.mkdirSync(destDir,err =>{
+        if(err) throw err
+    })
+    fs.mkdirSync(`${destDir}/notes`,err => {
+        if(err) throw err
+    })
+    fs.mkdirSync(`${destDir}/code`,err => {
+        if(err) throw err
+    })
+}
+
+function getJsonFiles(jsonPath){
+    let jsonFiles = [];
+    function findJsonFile(path){
+        let files = fs.readdirSync(path);
+        files.forEach(function (item, index) {
+            let fPath = join(path,item);
+            let stat = fs.statSync(fPath);
+            if(stat.isDirectory() === true) {
+                findJsonFile(fPath);
+            }
+            if (stat.isFile() === true) { 
+              jsonFiles.push(fPath);
+            }
+        });
+    }
+    findJsonFile(jsonPath);
+    return jsonFiles;
+}
 
 inquirer.prompt([
     {
@@ -16,5 +47,26 @@ inquirer.prompt([
         message: 'homework path?'
     }
 ]).then(answers => {
-    console.log(answers,__dirname);
+    const tmplDir = join(__dirname, 'templates')
+    const destDir = answers.outPath
+    const newDestDir = join(process.cwd(),destDir)
+    const tmplFileList = getJsonFiles(tmplDir);
+    fs_mkdirs(destDir)
+    fs.exists(join(newDestDir,'notes'),exists => {
+        if(exists){
+            // 该文件或目录存在！
+            tmplFileList.forEach(file => {
+                ejs.renderFile(file,answers,(err,res) => {
+                    if(err) throw err
+                    const filePath  = file.split('templates/')[1]
+                    fs.writeFileSync(join(newDestDir,filePath),res)
+                    
+                })
+            })
+        }
+        else{
+            // 该文件或目录不存在
+            fs_mkdirs(destDir)
+        }
+    });
 })
